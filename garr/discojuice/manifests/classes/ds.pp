468ee838f1dcf343f177f7c0f7ff0cd36daf12cf
Andrea Biancini <andrea.biancini@gmail.com>
2014-06-03 17:07:48 +0200

    Fixed errors

--- garr/discojuice/manifests/classes/ds.pp
+++ garr/discojuice/manifests/classes/ds.pp
@@ -19,38 +19,38 @@ class discojuice::ds(
       require => File['/var/www/disco'],
       notify  => Service['httpd'];
     
-    '/var/www/disco/css':
-      ensure  => directory,
-      source  => ['puppet:///modules/discojuice/css/discojuice.css'],
+    '/var/www/disco/css/':
+      ensure       => directory,
+      source       => ['puppet:///modules/discojuice/css/discojuice.css'],
       sourceselect => all,
-      require => File['/var/www/disco'];  
+      require      => File['/var/www/disco'];  
     
-    '/var/www/disco/js':
-      ensure  => directory,
-      source  => [
+    '/var/www/disco/js/':
+      ensure       => directory,
+      source       => [
         'puppet:///modules/discojuice/js/discojuice-stable.min.js',
         'puppet:///modules/discojuice/js/idpdiscovery.js',
         'puppet:///modules/discojuice/js/jquery.min.js',
         'puppet:///modules/discojuice/js/jquery.min.map',
       ],
       sourceselect => all,
-      require => File['/var/www/disco'];
+      require      => File['/var/www/disco'];
       
-    '/var/www/disco/flags':
-      ensure  => directory,
-      source  => ['puppet:///modules/discojuice/flags/it.png'],
+    '/var/www/disco/flags/':
+      ensure       => directory,
+      source       => ['puppet:///modules/discojuice/flags/it.png'],
       sourceselect => all,
-      require => File['/var/www/disco'];
+      require      => File['/var/www/disco'];
       
-    '/var/www/disco/images':
-      ensure  => directory,
-      source  => [
+    '/var/www/disco/images/':
+      ensure       => directory,
+      source       => [
         'puppet:///modules/discojuice/images/error.png',
         'puppet:///modules/discojuice/images/info.png',
         'puppet:///modules/discojuice/images/spinning.gif',
       ],
       sourceselect => all,
-      require => File['/var/www/disco'];
+      require      => File['/var/www/disco'];
   }
   
 }
