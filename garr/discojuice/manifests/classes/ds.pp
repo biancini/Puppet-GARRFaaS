ad622796e3047084023e6e3bbfe4f702f087070a
Andrea Biancini <andrea.biancini@gmail.com>
2014-06-03 17:14:32 +0200

    Fixed errors

--- garr/discojuice/manifests/classes/ds.pp
+++ garr/discojuice/manifests/classes/ds.pp
@@ -4,53 +4,89 @@ class discojuice::ds(
   $technicalGivenName  = 'administrator',
   $technicalSurName    = 'name',
   $federation_name     = 'Test Federation',
+  $test_federation     = true,
 ) {
-  
-  $disco_feed_url = $discojuice::prerequisites::my_discofeed_url
   
   file {
     '/var/www/disco':
       ensure  => directory,
       require => Class['apache'];
       
-    '/var/www/disco/index.php':
+    '/var/www/disco/css':
+      ensure  => directory,
+      require => File['/var/www/disco'];
+      
+    '/var/www/disco/js':
+      ensure  => directory,
+      require => File['/var/www/disco'];
+      
+    '/var/www/disco/images':
+      ensure  => directory,
+      require => File['/var/www/disco'];
+      
+    '/var/www/disco/flags':
+      ensure  => directory,
+      require => File['/var/www/disco'];
+      
+    '/var/www/disco/css/discojuice.css':
       ensure  => present,
-      content => template('discojuice/index.php.erb'),
-      require => File['/var/www/disco'],
+      source  => 'puppet:///modules/discojuice/css/discojuice.css',
+      require => File['/var/www/disco/css'],
       notify  => Service['httpd'];
-    
-    '/var/www/disco/css/':
-      ensure       => directory,
-      source       => ['puppet:///modules/discojuice/css/discojuice.css'],
-      sourceselect => all,
-      require      => File['/var/www/disco'];  
-    
-    '/var/www/disco/js/':
-      ensure       => directory,
-      source       => [
-        'puppet:///modules/discojuice/js/discojuice-stable.min.js',
-        'puppet:///modules/discojuice/js/idpdiscovery.js',
-        'puppet:///modules/discojuice/js/jquery.min.js',
-        'puppet:///modules/discojuice/js/jquery.min.map',
-      ],
-      sourceselect => all,
-      require      => File['/var/www/disco'];
       
-    '/var/www/disco/flags/':
-      ensure       => directory,
-      source       => ['puppet:///modules/discojuice/flags/it.png'],
-      sourceselect => all,
-      require      => File['/var/www/disco'];
+    '/var/www/disco/js/discojuice-stable.min.js':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/js/discojuice-stable.min.js',
+      require => File['/var/www/disco/js'],
+      notify  => Service['httpd'];
       
-    '/var/www/disco/images/':
-      ensure       => directory,
-      source       => [
-        'puppet:///modules/discojuice/images/error.png',
-        'puppet:///modules/discojuice/images/info.png',
-        'puppet:///modules/discojuice/images/spinning.gif',
-      ],
-      sourceselect => all,
-      require      => File['/var/www/disco'];
+    '/var/www/disco/js/idpdiscovery.js':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/js/idpdiscovery.js',
+      require => File['/var/www/disco/js'],
+      notify  => Service['httpd'];
+      
+    '/var/www/disco/js/jquery.min.js':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/js/jquery.min.js',
+      require => File['/var/www/disco/js'],
+      notify  => Service['httpd'];
+      
+    '/var/www/disco/js/jquery.min.map':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/js/jquery.min.map',
+      require => File['/var/www/disco/js'],
+      notify  => Service['httpd'];
+      
+    '/var/www/disco/index.php':
+	    ensure  => present,
+	    content => template('discojuice/index.php.erb'),
+	    require => File['/var/www/disco'],
+	    notify  => Service['httpd'];
+	    
+	  '/var/www/disco/flags/it.png':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/flags/it.png',
+      require => File['/var/www/disco/flags'],
+      notify  => Service['httpd'];
+      
+    '/var/www/disco/images/error.png':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/images/error.png',
+      require => File['/var/www/disco/images'],
+      notify  => Service['httpd'];
+      
+    '/var/www/disco/images/info.png':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/images/info.png',
+      require => File['/var/www/disco/images'],
+      notify  => Service['httpd'];
+      
+    '/var/www/disco/images/spinning.gif':
+      ensure  => present,
+      source  => 'puppet:///modules/discojuice/images/spinning.gif',
+      require => File['/var/www/disco/images'],
+      notify  => Service['httpd'];
   }
   
 }
