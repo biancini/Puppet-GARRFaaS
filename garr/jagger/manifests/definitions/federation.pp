eb455584ab3992b036a8a7c72e66dabe32500d97
Andrea Biancini <andrea.biancini@gmail.com>
2014-06-23 15:02:10 +0200

    Create federation in Jagger

--- garr/jagger/manifests/definitions/federation.pp
+++ garr/jagger/manifests/definitions/federation.pp
@@ -12,15 +12,10 @@ define jagger::federation (
     password          => $rootpw,
     dbname            => 'rr3',
     query_check_empty => "SELECT * FROM federation WHERE name = '${federation_name}'",
-    sql => [join(['START TRANSACTION;',
-                  '',
-                  'INSERT INTO federation (name, urn, publisher, description, is_active, is_protected, is_public, is_lexport, is_local, attrreq_inmeta, tou, owner)',
-                  "  VALUES ('${federation_name}', 'urn:mace:${domain_name}:${federation_id}', NULL, '${federation_description}', '1', 0, 1, 0, 1, 0, '${federation_tou}', 'admin');",
-                  '',
-                  'INSERT INTO acl_resource (resource, description, type, default_value, parent_id)',
-                  '  VALUES (CONCATENATE(\'f_\', LAST_INSERT_ID()), NULL, \'federation\', \'read\', \'6\');',
-                  '',
-                  'COMMIT;'], ' ')];      
+    sql => [join(['INSERT INTO federation (name, urn, publisher, description, is_active, is_protected, is_public, is_lexport, is_local, attrreq_inmeta, tou, owner)',
+                  "  VALUES ('${federation_name}', 'urn:mace:${domain_name}:${federation_id}', NULL, '${federation_description}', '1', 0, 1, 0, 1, 0, '${federation_tou}', 'admin')"], ' '),
+            join(['INSERT INTO acl_resource (resource, description, type, default_value, parent_id)',
+                  '  VALUES (CONCATENATE(\'f_\', LAST_INSERT_ID()), NULL, \'federation\', \'read\', \'6\')'], ' ')];      
   }
 
 }
