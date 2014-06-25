3902bb041a666f012c07c232c51393d634aa98e2
Andrea Biancini <andrea.biancini@gmail.com>
2014-06-25 15:56:30 +0200

    Fix

--- garr/mda/manifests/classes/prerequisites.pp
+++ garr/mda/manifests/classes/prerequisites.pp
@@ -12,4 +12,31 @@ class mda::prerequisites() {
     creates => ['/opt/ukf-meta'];
   }
   
+  file {
+    '/etc/apache2/sites-available/mda.conf':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => [join(['   Alias /mda /opt/ukf-meta/mdx/it_idem/md-out',
+                        '',
+                        '   <Directory /opt/ukf-meta/mdx/it_idem/md-out>',
+                        '     Options Indexes FollowSymLinks MultiViews',
+                        '     AllowOverride All',
+                        '     Order allow,deny',
+                        '     allow from all',
+                        '   </Directory>'], "\n")],
+      require => Class['apache'],
+      notify  => Exec['shib2-apache-restart'];
+
+    '/etc/apache2/sites-enabled/mda.conf':
+      ensure  => link,
+      target  => '/etc/apache2/sites-available/mda.conf',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => File['/etc/apache2/sites-available/mda.conf'],
+      notify  => Exec['shib2-apache-restart'];
+  }
+  
 }
